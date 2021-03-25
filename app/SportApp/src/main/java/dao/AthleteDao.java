bf2efb7043eab7f3a3fddaa0f468e11ab0f4b866
Ioatsi <j.tsilikas.j@gmail.com>
2021-03-25 15:37:46 +0200

    Revert "Add AthleteDao methods"
    This reverts commit 0282b5aefac59efb23ea01d647a753f2f813ffb7.

--- app/SportApp/src/main/java/dao/AthleteDao.java
+++ app/SportApp/src/main/java/dao/AthleteDao.java
@@ -1,15 +1,4 @@
 package dao;
-@Dao
+
 public interface AthleteDao {
-    @Insert
-    public void insertAthlete(Athlete athlete);
-
-    @Update
-    public void updateAthlete(Athlete athlete);
-
-    @Delete
-    public void deleteAthlete(Athlete athlete);
-
-    @Query("SELECT * FROM Athlete")
-    public Athlete[] loadAllAthletes; 
 }
