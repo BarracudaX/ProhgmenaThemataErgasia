3c0958d6fdff0d7535b160305b2187304354ac8a
Ioatsi <j.tsilikas.j@gmail.com>
2021-03-25 15:35:03 +0200

    Add Athlete constructor & get methods

--- app/SportApp/src/main/java/domain/Athlete.java
+++ app/SportApp/src/main/java/domain/Athlete.java
@@ -1,4 +1,52 @@
 package domain;
+import android.room;
 
+@Entity
 public final class Athlete {
+    @PrimaryKey
+    public int AthleteCode;
+    public String Name;
+    public String Surname;
+    public String City;
+    public String Country;
+    public int SportCode;
+    public LocalDate DateOfBirth;
+
+    public Athlete(int athleteCode, String name, String surname, String city, String country, int sportCode, LocalDate dateOfBirth) {
+        AthleteCode = athleteCode;
+        Name = name;
+        Surname = surname;
+        City = city;
+        Country = country;
+        SportCode = sportCode;
+        DateOfBirth = dateOfBirth;
+    }
+
+    public int getAthleteCode() {
+        return AthleteCode;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public int getSportCode() {
+        return SportCode;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return DateOfBirth;
+    }
 }
