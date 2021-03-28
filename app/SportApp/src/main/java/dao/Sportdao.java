470ef34ea55e98d1057455b77815299afc225927
Margarita <markoutra@gmail.com>
2021-03-28 18:40:07 +0300

    Update Sportdao.java

--- app/SportApp/src/main/java/dao/Sportdao.java
+++ app/SportApp/src/main/java/dao/Sportdao.java
@@ -1,4 +1,16 @@
 package dao;
 
 public interface SportDao {
+
+    @Query("SELECT * FROM Sport")
+    List<Sport> loadAllSports();
+
+    @Insert
+    void insertSport(Sport sport);
+
+    @Update
+    void updateSport(Sport sport);
+
+    @Delete
+    void deleteSport(Sport sport);
 }
