f468c3e549f28249303f60a10810dfe7a240da68
Barracuda <dimitrijchannel@gmail.com>
2021-04-08 13:26:34 +0300

    Changes: 1)Added TeamMainFragment fragment that contains the bottom navigation and will display all the inserted teams. 2)Added icons for menu items. 3)Changed name of nav_graph.xml to main_activity_nav_graph.xml. 4)Changed the navigation graph and the menus.

---
+++ app/SportApp/src/main/java/com/sport/sportapp/fragments/TeamMainFragment.java
@@ -0,0 +1,44 @@
+package com.sport.sportapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.sport.sportapp.R;
+import com.sport.sportapp.databinding.FragmentTeamMainBinding;
+import com.sport.sportapp.databinding.FragmentWelcomeBinding;
+
+public class TeamMainFragment extends Fragment {
+
+    private  FragmentTeamMainBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentTeamMainBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        NavHostFragment navHostFragment = (NavHostFragment) getParentFragment();
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView = binding.bottomNavigationView;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            navController.navigate(item.getItemId());
+            return true;
+        });
+    }
+}
