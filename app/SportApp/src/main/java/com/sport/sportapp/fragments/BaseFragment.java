043d3d968e84e755b66d1245ac50371618c7378a
Barracuda <dimitrijchannel@gmail.com>
2021-04-09 17:04:18 +0300

    Revert "Changes:"
    This reverts commit 546f1e8a5ca5ed45e6d1d00b997a18637b9b5855.
    
    Reverting back.

--- app/SportApp/src/main/java/com/sport/sportapp/fragments/BaseFragment.java
+++ app/SportApp/src/main/java/com/sport/sportapp/fragments/BaseFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,18 +15,23 @@ import viewmodels.MainActivityViewModel;
 public abstract class BaseFragment extends Fragment {
 
     private MainActivityViewModel viewModel;
+    private NavController navController;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = new ViewModelProvider(getActivity()).get(MainActivityViewModel.class);
+        navController = ((NavHostFragment)getParentFragment()).getNavController();
+        viewModel.navigationToEvent().observe(this, request -> {
+            navController.navigate(request.getDestinationId(),request.getBundle());
+        });
+        viewModel.navigateBackEvent().observe(this,ignored -> navController.navigateUp());
     }
 
     protected void configureBottomNavigation(BottomNavigationView bottomNavigationView) {
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            viewModel.navigateTo(item.getItemId(),null);
+            navController.navigate(item.getItemId());
             return true;
         });
     }
-
 }
--- app/SportApp/src/main/java/viewmodels/MainActivityViewModel.java
+++ app/SportApp/src/main/java/viewmodels/MainActivityViewModel.java
@@ -4,10 +4,10 @@ import android.app.Application;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,19 +19,19 @@ import dao.TeamDao;
 import domain.SportIdNameModel;
 import domain.Team;
 
-public class MainActivityViewModel extends ViewModel {
+public class MainActivityViewModel extends AndroidViewModel {
 
     private final TeamDao teamDao;
     private final SportDao sportDao;
     private final AthleteDao athleteDao;
-    private final NavController navController;
-
     private LiveData<List<SportIdNameModel>> sportIdsAndNames ;
     private LiveData<List<Team>> teams;
     private MutableLiveData<LocalDate> pickedDate = new MutableLiveData<>();
+    private SingleLiveEvent<NavigationRequest> navDestinationRequest = new SingleLiveEvent<>();
+    private SingleLiveEvent<Void> navigateBackRequest = new SingleLiveEvent<>();
 
-    public MainActivityViewModel(@NonNull Application application, NavController navController) {
-        this.navController = navController;
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
         AppDatabase appDatabase = AppDatabase.getInstance(application);
         teamDao = appDatabase.teamDao();
         sportDao = appDatabase.sportDao();
@@ -72,12 +72,20 @@ public class MainActivityViewModel extends ViewModel {
         return teamDao.findById(teamId);
     }
 
+    public LiveData<NavigationRequest> navigationToEvent() {
+        return navDestinationRequest;
+    }
+
     public void navigateTo(int destinationId, Bundle bundleRequest) {
-        navController.navigate(destinationId,bundleRequest);
+        navDestinationRequest.setValue(new NavigationRequest(destinationId,bundleRequest));
+    }
+
+    public LiveData<Void> navigateBackEvent() {
+        return navigateBackRequest;
     }
 
     public void navigateBack() {
-        navController.navigateUp();
+        navigateBackRequest.setValue(null);
     }
 
     public void deleteTeamById(long id) {
